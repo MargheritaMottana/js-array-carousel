577af1fc82e4592c1e8e036a49cf6316297ae0d2
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-18 17:18:07 +0200

    scrittura html e css + inizio scrittura step js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('hello')
+/*
+
+Consegna:
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
+
+Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+MILESTONE 3
+Al click dell'utente sulle frecce, il programma cambierà l'immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+*/
+
+/*
+
+Step 1  - Scrivere html con immagini, poi commentare la sezione
+        - scrivere in html bottoni per scorrere
+
+Step 2 - In css rendere invisibili tutte le immagini
+       - Creare in css una classe che mi permetta di rendere visibile un'immagine
+
+Step 3 - Scrivere in JS, un array con tutte le immagini che si vedranno nel carosello
+
+Step 4 - Scrivere Js un ciclo che legga tutte le immagini dell'array
+
+Step 5 - ascoltando il click su un bottone, riprendendo la classe css, rendere visibile l'immegine successiva o precedente
+
+*/
